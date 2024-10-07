80c67cca494e4e40c5710e790d5226f7f0705e30
harantsuki <anyazosen@gmail.com>
2024-10-08 03:20:31 +0700

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/harantsuki/", // Tambahkan ini
+  base: "/Chlyn-Portfolio/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom"],
+        },
+      },
+    },
+  },
 });
